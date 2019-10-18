d25126e21d572c3e8a18f7c309dcfd84426d883f
godkkoo <godkkoo@gmail.com>
2019-10-18 12:55:25 +0900

    dd

--- diTest02/src/com/bit/exam03/MessageBeanEn.java
+++ diTest02/src/com/bit/exam03/MessageBeanEn.java
@@ -5,7 +5,7 @@ public class MessageBeanEn implements MessageBean{
 	@Override
 	public void sayHello(String name) {
 		System.out.println("영어" + name);
-		
+		System.out.println("영어" + name);
 	}
 	
 }
